1814776a553d9f34fa7a21ec63134cc8064bd2b5
bcy <1250030585@qq.com>
2018-02-14 22:47:47 +0800

    zaishiyici

--- src/com/company/Solution_15.java
+++ src/com/company/Solution_15.java
@@ -5,11 +5,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Solution_15 {
-    public List<List<Integer>> threeSum(int[] num) {
-        Arrays.sort(num);
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList<List<Integer>>();
-        for (int i = 0; i < num.length - 2; i++) {
-            
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lo = i + 1;
+                int hi = nums.length - 1;
+                int sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    } else {
+                        if (nums[lo] + nums[hi] < sum) lo++;
+                        else hi--;
+                    }
+                }
+            }
         }
+        return res;
     }
 }
