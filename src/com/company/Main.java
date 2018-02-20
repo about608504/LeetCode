823feb3573ea8e3a64ca431b4af1b6275e7e57be
bcy <1250030585@qq.com>
2018-02-20 23:11:44 +0800

    2018.2.20

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
+import java.math.BigInteger;
 public class Main {
 
     public static void main(String[] args) {
-        Solution_17_BFS sol = new Solution_17_BFS();
-        System.out.println(sol.letterCombinations("234"));
-
+        System.out.println("lalala");
     }
 }
